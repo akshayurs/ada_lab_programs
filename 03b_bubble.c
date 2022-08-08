2253b2cd7473a55fd236b8a9b7176292308fc47f
Akshay Urs <akshayursm@gmail.com>
2022-08-08 21:47:09 +0530

    new programs added

---
+++ 03b_bubble.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define START 10
+#define END 100
+#define INCR 10
+void bubblesort(int arr[], int n, char filename[])
+{
+    int count = 0;
+    int i, j, temp, flag;
+    for (i = 0; i < n - 1; i++)
+    {
+        flag = 1;
+        for (j = 0; j < n - i - 1; j++)
+        {
+            count++;
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                flag = 0;
+            }
+        }
+        if (flag)
+            break;
+    }
+    FILE *fp = fopen(filename, "a");
+    fprintf(fp, "%d\t", n);
+    fprintf(fp, " %d\n", count);
+}
+void main()
+{
+    int i, n;
+    system("rm -f count_*.txt");
+    srand(time(NULL));
+    for (n = START; n <= END; n += INCR)
+    {
+        int arr[n];
+        for (i = 0; i < n; i++)
+            arr[i] = i;
+        bubblesort(arr, n, "count_best.txt");
+        for (i = 0; i < n; i++)
+            arr[i] = n - i;
+        bubblesort(arr, n, "count_worst.txt");
+        for (i = 0; i < n; i++)
+            arr[i] = rand() % 5;
+        bubblesort(arr, n, "count_average.txt");
+    }
+}
---
+++ 03c_insertion.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define START 10
+#define END 100
+#define INCR 10
+void insertionsort(int arr[], int n, char filename[])
+{
+    int count = 0;
+    int i, j, k;
+    for (i = 1; i < n; i++)
+    {
+        k = arr[i];
+        for (j = i - 1; j >= 0; j--)
+        {
+            count++;
+            if (arr[j] < k)
+                break;
+            arr[j + 1] = arr[j];
+        }
+        arr[j + 1] = k;
+    }
+    FILE *fp = fopen(filename, "a");
+    fprintf(fp, "%d\t", n);
+    fprintf(fp, " %d\n", count);
+}
+void main()
+{
+    int i, n;
+    system("rm -f count_*.txt");
+    srand(time(NULL));
+    for (n = START; n <= END; n += INCR)
+    {
+        int arr[n];
+        for (i = 0; i < n; i++)
+            arr[i] = i;
+        insertionsort(arr, n, "count_best.txt");
+        for (i = 0; i < n; i++)
+            arr[i] = n - i;
+        insertionsort(arr, n, "count_worst.txt");
+        for (i = 0; i < n; i++)
+            arr[i] = rand() % 5;
+        insertionsort(arr, n, "count_average.txt");
+    }
+}
