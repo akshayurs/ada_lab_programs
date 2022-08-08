d90b9b18cca1fd6e323051723c1bf64a01253944
Akshay Urs <akshayursm@gmail.com>
2022-08-08 23:09:50 +0530

    new program added

---
+++ 04a_string.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define START 10
+#define END 100
+#define INCR 10
+
+void patternmatch(int text[], int pattern[], int n, int m, char filename[])
+{
+    int count = 0, i, j;
+    for (i = 0; i <= n - m; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            count++;
+            if (pattern[j] != text[i + j])
+                break;
+        }
+        if (j == n)
+            break;
+    }
+    FILE *fp = fopen(filename, "a");
+    fprintf(fp, "%d\t", n);
+    fprintf(fp, " %d\n", count);
+}
+void main()
+{
+    int i, n, m = 4, text[END] = {0}, pattern[m];
+    system("rm -f count_*.txt");
+    srand(time(NULL));
+    for (n = START; n <= END; n += INCR)
+    {
+        for (i = 0; i < m; i++)
+            pattern[i] = 0;
+        pattern[0] = 1;
+        patternmatch(text, pattern, n, m, "count_best.txt");
+        pattern[0] = 0;
+        pattern[m - 1] = 1;
+        patternmatch(text, pattern, n, m, "count_worst.txt");
+        for (i = 0; i < m; i++)
+            pattern[i] = rand() % 2;
+        patternmatch(text, pattern, n, m, "count_average.txt");
+    }
+}
