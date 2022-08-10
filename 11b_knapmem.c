d6ddaa3946fa8f6fcd9c3647454b1ba2733f037c
Akshay Urs <akshayursm@gmail.com>
2022-08-10 22:20:51 +0530

    bug fix

--- 11b_knapmem.c
+++ 11b_knapmem.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 int table[100][100];
-int v[100], w[100];
+int val[100], wt[100];
 int max(int a, int b) { return (a > b) ? a : b; }
 
-int Mem_Fun_knapsack(int i, int j)
+int knapsack(int i, int j)
 {
-    int value;
-    if (table[i][j] == -1)
-    {
-        if (j < w[i])
-        {
-            value = Mem_Fun_knapsack(i - 1, j);
-        }
-        else
-        {
-            value = max(Mem_Fun_knapsack(i - 1, j),
-                        v[i] +
-                            Mem_Fun_knapsack(i - 1, j - w[i]));
-        }
-        table[i][j] = value;
-    }
+    if (i < 0)
+        return 0;
+    if (table[i][j] != -1)
+        return table[i][j];
+    if (wt[i - 1] > j)
+        table[i][j] = knapsack(i - 1, j);
+    else
+        table[i][j] = max(val[i - 1] + knapsack(i - 1, j - wt[i - 1]), knapsack(i - 1, j));
     return table[i][j];
 }
 
@@ -33,20 +26,10 @@ int main()
     for (int i = 0; i < n; i++)
     {
         printf("\nEnter value and weight for %d item: ", i + 1);
-        scanf("%d %d", &v[i], &w[i]);
+        scanf("%d %d", &val[i], &wt[i]);
     }
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= capacity; j++)
             table[i][j] = -1;
-    for (int j = 0; j <= capacity; j++)
-        table[0][j] = 0;
-    for (int i = 0; i <= n; i++)
-        table[i][0] = 0;
-    printf("Max value: %d\n", Mem_Fun_knapsack(n, capacity));
-    for (int i = 0; i <= n; i++)
-    {
-        for (int j = 0; j <= capacity; j++)
-            printf("%d  ", table[i][j]);
-        printf("\n");
-    }
+    printf("Max value: %d\n", knapsack(n, capacity));
 }
